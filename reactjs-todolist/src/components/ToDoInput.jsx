c36ab8be47a0219280667ab1267d3d4048167176
Tirth Bhavesh Shah <tshah@csuchico.edu>
2024-10-24 14:29:02 -0700

    fixed the file name

--- reactjs-todolist/src/components/ToDoInput.jsx
+++ reactjs-todolist/src/components/ToDoInput.jsx
@@ -1,16 +1,24 @@
-import { useState } from "react"
+import { useState } from "react";
 
 export default function TodoInput(props) {
-    const { handleAddTodos, todoValue, setTodoValue } = props
-    return (
-        <header>
-            <input value={todoValue} onChange={(e) => {
-                setTodoValue(e.target.value)
-            }} placeholder="Enter todo..." />
-            <button onClick={() => {
-                handleAddTodos(todoValue)
-                setTodoValue('')
-            }}>Add</button>
-        </header>
-    )
+  const { handleAddTodos, todoValue, setTodoValue } = props;
+  return (
+    <header>
+      <input
+        value={todoValue}
+        onChange={(e) => {
+          setTodoValue(e.target.value);
+        }}
+        placeholder="Enter todo..."
+      />
+      <button
+        onClick={() => {
+          handleAddTodos(todoValue);
+          setTodoValue("");
+        }}
+      >
+        Add
+      </button>
+    </header>
+  );
 }
